63623188b3e0598897d238b18e0697c94f27bd70
Iamsushantgautam <sushantkumarlko2507@gmail.com>
2026-01-22 13:59:44 +0530

    profile 2

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -253,7 +253,37 @@ const Profile = ({ usernameOverride }) => {
                                     </div>
                                 </a>
                             ))}
-                        </div>
+                        </div><br />
+
+                        {/* Important Links */}
+                        {profile?.importantLinks && profile.importantLinks.length > 0 && (
+                            <div className="profile-links-section">
+                                {profile.importantLinks.map((link, idx) => (
+                                    <a
+                                        key={idx}
+                                        href={link.url}
+                                        target="_blank"
+                                        rel="noreferrer"
+                                        className="profile-link-item"
+                                    >
+                                        <div className="profile-link-item__content">
+                                            <div className="profile-link-item__icon">
+                                                <ExternalLink size={20} />
+                                            </div>
+                                            <div className="profile-link-item__text">
+                                                <div className="profile-link-item__title">{link.title}</div>
+                                                <div className="profile-link-item__url">{link.url}</div>
+                                            </div>
+                                        </div>
+                                        <ChevronRight className="profile-link-item__arrow" size={20} />
+                                    </a>
+                                ))}
+                            </div>
+                        )}
+
+
+
+
 
                         {/* Social Media Section */}
                         {profile?.socialLinks && Object.keys(profile.socialLinks).some(key => profile.socialLinks[key]) && (
